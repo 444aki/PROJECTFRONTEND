63798541f3808a7992d6c57aa9538d34058bec28
444aki <akihere44@gmail.com>
2023-11-19 17:12:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { HashRouter,Routes,Route } from 'react-router-dom';
+import Nav from './components/Nav';
+import CreateEmp from './components/CreateEmp';
+import EmpList from './components/EmpList';
+import EditEmp from './components/EditEmp';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+      <HashRouter>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<CreateEmp />} />
+          <Route path="/create-employees" element={<CreateEmp />} />
+          <Route path="/employees-list" element={<EmpList />} />
+          <Route path="/edit-employees/:id" element={<EditEmp />} />
+        </Routes>
+      </HashRouter>
     </div>
   );
 }
